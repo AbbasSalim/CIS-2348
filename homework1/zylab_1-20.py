afbe90f5f36ef9bc7952eee28298b7379b442c95
AbbasSalim <143136178+AbbasSalim@users.noreply.github.com>
2023-09-14 17:19:13 -0500

    Zylab 1.20

---
+++ homework1/zylab_1-20.py
@@ -0,0 +1,16 @@
+# Abbas Salim
+# 2026396
+
+user_num1 = int(input('Enter integer:'))
+print()
+print(f'You entered: {user_num1}')
+user_num1_sq = user_num1 * user_num1
+user_num1_cube = user_num1 ** 3
+print(f'{user_num1} squared is {user_num1_sq}')
+print(f'And {user_num1} cubed is {user_num1_cube} !!')
+user_num2 = int(input('Enter another integer:'))
+print()
+user_num2_add = user_num2 + user_num1
+user_num2_mul = user_num1 * user_num2
+print(f'{user_num1} + {user_num2} is {user_num2_add}')
+print(f'{user_num1} * {user_num2} is {user_num2_mul}')
